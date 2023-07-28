7210246a6d77753302e8849b67346872a352d80b
ramshers <npe@abc.com>
2023-07-29 03:19:46 +0530

    basic code added

---
+++ src/main/java/com/example/jdkhttpclientlogging/JdkHttpclientLoggingApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class JdkHttpclientLoggingApplication {
+
+  @SneakyThrows
+  public static void main(String[] args) {
+
+    SpringApplication.run(com.example.demo.JdkHttpclientLoggingApplication.class, args);
+
+
+    log.info("default charset: " + Charset.defaultCharset());
+
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI("https://postman-echo.com/get"))
+        .version(HttpClient.Version.HTTP_2)
+        .GET()
+        .build();
+    HttpResponse<String> response = HttpClient.newHttpClient()
+        .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+    System.err.println("response: " + response);
+
+  }
+
+}
---
+++ src/test/java/com/example/jdkhttpclientlogging/JdkHttpclientLoggingApplicationTests.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+@Slf4j
+public class JdkHttpclientLoggingApplicationTests {
+
+  @Test
+  void contextLoads() throws URISyntaxException, IOException, InterruptedException {
+
+    System.err.println("default charset: " + Charset.defaultCharset());
+
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI("https://postman-echo.com/get"))
+        .version(HttpClient.Version.HTTP_2)
+        .GET()
+        .build();
+    HttpResponse<String> response = HttpClient.newHttpClient()
+        .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+    System.err.println("response: " + response);
+
+    Assert.notNull("a");
+
+  }
+
+}
